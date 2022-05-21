e8ae5bc7c7d66a127008e62a8bc362a41773186a
khaledalics22 <khaled.ali.06.10@gmail.com>
2022-05-21 05:36:01 +0200

    add hadoop executing record

--- script.sh
+++ script.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/bash
 export PDSH_RCMD_TYPE=ssh
-stop-all.sh
 start-all.sh
 /usr/local/hadoop/bin/hadoop dfsadmin -safemode leave
-#hadoop fs -mkdir /PyHadoop
-#hadoop fs -mkdir /PyHadoop/Input
+hadoop fs -mkdir /PyHadoop
+hadoop fs -mkdir /PyHadoop/Input
 hdfs dfs -rm -r /PyHadoop/Input/*
 hdfs dfs -rm -r /PyHadoop/Output
 hdfs dfs -put os_data.csv /PyHadoop/Input
-hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.2.jar -file /home/khalid/Desktop/FY/BDproject/Project/mapper.py -mapper "/home/khalid/Desktop/FY/BDproject/Project/mapper.py 0.3 100" -file ~/Desktop/FY/BDproject/Project/reducer.py -reducer ~/Desktop/FY/BDproject/Project/reducer.py -input /PyHadoop/Input -output /PyHadoop/Output
+hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.2.jar -file ~/Desktop/project/mapper.py -mapper "/home/khalid/Desktop/project/mapper.py 0.3 100" -file ~/Desktop/project/reducer.py -reducer ~/Desktop/project/reducer.py -input /PyHadoop/Input -output /PyHadoop/Output
+rm part-00000
 hdfs dfs -get /PyHadoop/Output/part-00000 ~/Desktop/project/
+stop-all.sh
